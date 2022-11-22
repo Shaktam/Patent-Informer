a337d9e468cbe6dcd4135d6a395b45dd7bf0f6e8
Shakti Thandavan <srishakti193@gmail.com>
2022-11-22 22:57:29 +0100

    modify gitignore

--- infrastructure/patent_watch.tf
+++ infrastructure/patent_watch.tf
@@ -6,7 +6,7 @@ resource "aws_lambda_function" "patent_notifier" {
   timeout       = 300
   runtime       = "python3.9"
   layers        = [aws_lambda_layer_version.requests_layer.arn]
-  source_code_hash = filebase64sha256("build/job_crawler.zip")
+  source_code_hash = filebase64sha256("patent_notifier.zip")
 
   environment {
     variables = {
@@ -17,29 +17,29 @@ resource "aws_lambda_function" "patent_notifier" {
 
 
 resource "aws_lambda_layer_version" "requests_layer" {
-  s3_bucket     = "job-notifier-src-bucket-21345"
+  s3_bucket     = "patent-notifier-bucket"
   s3_key        = "requests-layer.zip"
   layer_name    = "requests-layer"
 
   compatible_runtimes = ["python3.9"]
 }
 
-resource "aws_cloudwatch_event_rule" "every_five_minutes" {
-    name = "every-five-minutes"
-    description = "Fires every five minutes"
-    schedule_expression = "rate(5 minutes)"
+resource "aws_cloudwatch_event_rule" "every_twentyfour_hours" {
+    name = "every-twentyfour_hours"
+    description = "Fires every twentyfour_hours"
+    schedule_expression = "rate(24 hours)"
 }
 
-resource "aws_cloudwatch_event_target" "job_crawler_every_five_minutes" {
-    rule = aws_cloudwatch_event_rule.every_five_minutes.name
-    target_id = "crawl-jobs"
-    arn = aws_lambda_function.job_crawler.arn
+resource "aws_cloudwatch_event_target" "job_crawler_every_twentyfour_hours" {
+    rule = aws_cloudwatch_event_rule.every_twentyfour_hours.name
+    target_id = "patent_notify"
+    arn = aws_lambda_function.patent_notifier.arn
 }
 
-resource "aws_lambda_permission" "allow_cloudwatch_to_call_job_crawler" {
+resource "aws_lambda_permission" "allow_cloudwatch_to_call_patentname" {
     statement_id = "AllowExecutionFromCloudWatch"
     action = "lambda:InvokeFunction"
-    function_name = aws_lambda_function.job_crawler.function_name
+    function_name = aws_lambda_function.patent_notifier.function_name
     principal = "events.amazonaws.com"
-    source_arn = aws_cloudwatch_event_rule.every_five_minutes.arn
+    source_arn = aws_cloudwatch_event_rule.every_twentyfour_hours.arn
 }
